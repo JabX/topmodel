22f999dee87c469f3e6c1e43ecabf896c47cb13e
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2025-02-06 23:36:46 +0100

    [LS] Fix exception

--- TopModel.LanguageServer/DocumentLinkHandler.cs
+++ TopModel.LanguageServer/DocumentLinkHandler.cs
@@ -20,25 +20,30 @@ public class DocumentLinkHandler(ModelStore modelStore, ILanguageServerFacade fa
 
     public override async Task<DocumentLinkContainer?> Handle(DocumentLinkParams request, CancellationToken cancellationToken)
     {
-        var lockFile = new FileInfo(Path.Combine(_config.ConfigRoot, _config.LockFileName));
-        using var file = lockFile.OpenText();
-        var text = (await file.ReadToEndAsync()).Split('\n').ToList();
-        var indexOfGeneratedFiles = text.IndexOf(text.First(l => l.StartsWith("generatedFiles:")));
-        var end = text.Count();
-        List<DocumentLink> documentLinks = [];
-        var lockFileDir = Path.GetFullPath(Path.GetDirectoryName(request.TextDocument.Uri.Path)!.Trim(Path.DirectorySeparatorChar));
-
-        for (var i = indexOfGeneratedFiles + 1; i < end - 1; i++)
+        var lockFile = new FileInfo(Path.GetFullPath(Path.Combine(_config.ConfigRoot, _config.LockFileName)));
+        if (lockFile.Exists)
         {
-            var target = new Uri(Path.GetFullPath(Path.Combine(lockFileDir!, text[i]["  - ".Length..].Replace('/', Path.DirectorySeparatorChar))));
-            documentLinks.Add(new DocumentLink()
+            using var file = lockFile.OpenText();
+            var text = (await file.ReadToEndAsync()).Split('\n').ToList();
+            var indexOfGeneratedFiles = text.IndexOf(text.First(l => l.StartsWith("generatedFiles:")));
+            var end = text.Count();
+            List<DocumentLink> documentLinks = [];
+            var lockFileDir = Path.GetFullPath(Path.GetDirectoryName(request.TextDocument.Uri.Path)!.Trim(Path.DirectorySeparatorChar));
+
+            for (var i = indexOfGeneratedFiles + 1; i < end - 1; i++)
             {
-                Range = new OmniSharp.Extensions.LanguageServer.Protocol.Models.Range(new Position(i, 4), new Position(i, text[i].Length)),
-                Target = target
-            });
+                var target = new Uri(Path.GetFullPath(Path.Combine(lockFileDir!, text[i]["  - ".Length..].Replace('/', Path.DirectorySeparatorChar))));
+                documentLinks.Add(new DocumentLink()
+                {
+                    Range = new OmniSharp.Extensions.LanguageServer.Protocol.Models.Range(new Position(i, 4), new Position(i, text[i].Length)),
+                    Target = target
+                });
+            }
+
+            return new DocumentLinkContainer(documentLinks);
         }
 
-        return new DocumentLinkContainer(documentLinks);
+        return new DocumentLinkContainer();
     }
 
     protected override DocumentLinkRegistrationOptions CreateRegistrationOptions(DocumentLinkCapability capability, ClientCapabilities clientCapabilities)
