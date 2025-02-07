23741f79ca4143fce3304bb1a30d44f0ec36a3dd
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2025-02-07 10:06:29 +0100

    [LS] Correction liens dans workspace contenant plusieurs fichiers .lock

--- TopModel.LanguageServer/DocumentLinkHandler.cs
+++ TopModel.LanguageServer/DocumentLinkHandler.cs
@@ -20,27 +20,33 @@ public class DocumentLinkHandler(ModelStore modelStore, ILanguageServerFacade fa
 
     public override async Task<DocumentLinkContainer?> Handle(DocumentLinkParams request, CancellationToken cancellationToken)
     {
-        var lockFile = new FileInfo(Path.GetFullPath(Path.Combine(_config.ConfigRoot, _config.LockFileName)));
+        var lockFile = new FileInfo(Path.GetFullPath(request.TextDocument.Uri.Path.Replace('/', Path.DirectorySeparatorChar).Trim(Path.DirectorySeparatorChar)));
         if (lockFile.Exists)
         {
             using var file = lockFile.OpenText();
             var text = (await file.ReadToEndAsync()).Split('\n').ToList();
             var indexOfGeneratedFiles = text.IndexOf(text.First(l => l.StartsWith("generatedFiles:")));
-            var end = text.Count();
-            List<DocumentLink> documentLinks = [];
-            var lockFileDir = Path.GetFullPath(Path.GetDirectoryName(request.TextDocument.Uri.Path)!.Trim(Path.DirectorySeparatorChar));
-
-            for (var i = indexOfGeneratedFiles + 1; i < end - 1; i++)
+            if (indexOfGeneratedFiles > 0)
             {
-                var target = new Uri(Path.GetFullPath(Path.Combine(lockFileDir!, text[i]["  - ".Length..].Replace('/', Path.DirectorySeparatorChar))));
-                documentLinks.Add(new DocumentLink()
+                var end = text.Count();
+                List<DocumentLink> documentLinks = [];
+                var lockFileDir = Path.GetFullPath(Path.GetDirectoryName(request.TextDocument.Uri.Path)!.Trim(Path.DirectorySeparatorChar));
+                var lineStart = "  - ";
+                for (var i = indexOfGeneratedFiles + 1; i < end - 1; i++)
                 {
-                    Range = new OmniSharp.Extensions.LanguageServer.Protocol.Models.Range(new Position(i, 4), new Position(i, text[i].Length)),
-                    Target = target
-                });
-            }
+                    if (text[i].StartsWith(lineStart))
+                    {
+                        var target = new Uri(Path.GetFullPath(Path.Combine(lockFileDir!, text[i][lineStart.Length..].Replace('/', Path.DirectorySeparatorChar))));
+                        documentLinks.Add(new DocumentLink()
+                        {
+                            Range = new OmniSharp.Extensions.LanguageServer.Protocol.Models.Range(new Position(i, lineStart.Length), new Position(i, text[i].Length)),
+                            Target = target
+                        });
+                    }
+                }
 
-            return new DocumentLinkContainer(documentLinks);
+                return new DocumentLinkContainer(documentLinks);
+            }
         }
 
         return new DocumentLinkContainer();
@@ -50,7 +56,7 @@ public class DocumentLinkHandler(ModelStore modelStore, ILanguageServerFacade fa
     {
         return new DocumentLinkRegistrationOptions
         {
-            DocumentSelector = TextDocumentSelector.ForPattern($"**/{_config.LockFileName ?? "topmodel.lock"}")
+            DocumentSelector = TextDocumentSelector.ForPattern($"**/*.lock")
         };
     }
 }
